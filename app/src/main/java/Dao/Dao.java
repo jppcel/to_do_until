ccb995371389ec951ed5e00fd939c7969306fa09
motoneto <matt_ouriques@hotmail.com>
2017-09-22 20:37:47 -0300

    Add new activity

---
+++ app/src/main/java/Dao/Dao.java
@@ -0,0 +1,25 @@
+package Dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by fabricio on 22/09/17.
+ */
+
+public class Dao extends SQLiteOpenHelper {
+    public Dao(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
