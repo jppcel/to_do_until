9dffef49fc256adf0ae1431be53c3c79d8613d02
João Paulo Polles <joaopaulopolles@gmail.com>
2017-09-22 21:33:06 -0300

    Atualizacoes

---
+++ app/src/main/java/me/polles/to_do_until/SplashView.java
@@ -0,0 +1,13 @@
+package me.polles.to_do_until;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_view);
+    }
+}
